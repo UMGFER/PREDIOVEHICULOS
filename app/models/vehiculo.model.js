62d5a40901be252c6c00bca49e82d205c58461b6
JOSE RODRIGUEZ <jrodriguezg53@miumg.edu.gt>
2025-08-17 18:40:27 -0600

    modificacion de vehiculo

--- app/models/vehiculo.model.js
+++ app/models/vehiculo.model.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, Sequelize) => {
 // usamos el sequelize.defina para "definir" el nombre de la entity en la BD, en este caso "cliente"
 // Usamos type.Sequelize para definir el tipo de datos de cada atributo de la entidad 
     const Vehiculo = sequelize.define("vehiculo", {
-        id_vehiculp:{
+        id_vehiculo:{
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
